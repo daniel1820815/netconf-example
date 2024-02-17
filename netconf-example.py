df82506b3fe48d936af9e97d3af26cba064e2a4b
Daniel Kuhl <daniel@kuhlcloud.de>
2024-02-17 13:36:19 +0100

    added comments and nx-os device

--- netconf-example.py
+++ netconf-example.py
@@ -3,8 +3,10 @@ from ncclient import manager
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-devices = ["192.168.255.51", "192.168.255.52"]
+# List of devices
+devices = ["192.168.255.51", "192.168.255.52", "192.168.255.53"]
 
+# Loop through the devices and connect to it
 for device in devices:
     router = manager.connect(
         host=device,
@@ -13,7 +15,9 @@ for device in devices:
         hostkey_verify=False
         )
 
+    # Open config file for device as payload
     with open(f"{device}.xml") as file:
         payload = file.read()
 
+    # Send payload to running config using edit-config
     router.edit_config(payload, target="running")
