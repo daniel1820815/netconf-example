eb820b21c7cc355120562fd5f501396344c421dc
Daniel Kuhl <daniel@kuhlcloud.de>
2024-03-25 16:17:58 +0100

    working validation

--- netconf_validate.py
+++ netconf_validate.py
@@ -3,79 +3,65 @@ from ncclient import manager
 from lxml import etree
 import xml.etree.ElementTree as ET
 
-# List of devices IOSXE devices
+# List of devices
 devices = ["192.168.255.51", "192.168.255.52", "192.168.255.53"]
 
-iosxe_native_filter = '''
-    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-        <bgp-state-data xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-bgp-oper">
-            <neighbors>
-                <neighbor>
-                    <neighbor-id/>
-                    <session-state>fsm-established</session-state>
-                </neighbor>
-            </neighbors>
-        </bgp-state-data>
-    </filter>
-'''
 
-nxos_openconfig_filter = '''
-    <filter>
-        <network-instances xmlns="http://openconfig.net/yang/network-instance">
-            <network-instance>
-                <name>default</name>
-                <protocols>
-                    <protocol>
-                        <identifier xmlns:oc-pol-types="http://openconfig.net/yang/policy-types">oc-pol-types:BGP</identifier>
-                        <name>bgp</name>
-                        <bgp>
-                            <neighbors>
-                                <neighbor>
-                                    <state>
-                                        <session-state>ESTABLISHED</session-state>
-                                    </state>
-                                </neighbor>
-                            </neighbors>
-                        </bgp>
-                    </protocol>
-                </protocols>
-            </network-instance>
-        </network-instances>
-    </filter>
-'''
+def device_connect(dev):
+    '''Function to connect to the devices'''
 
-# Loop through the devices and connect to it
-for device in devices:
-    router = manager.connect(
-        host=device,
+    con = manager.connect(
+        host=dev,
         username="expert",
         password="1234QWer!",
         hostkey_verify=False
         )
+    return con
 
-    # Choose which filter to use
-    if device == "192.168.255.53":
-        netconf_filter = nxos_openconfig_filter
-        url = '{http://openconfig.net/yang/network-instance}'
-        root = 'root[0][0][1][0][2][0]'
-    else:
-        netconf_filter = iosxe_native_filter
-        url = '{http://cisco.com/ns/yang/Cisco-IOS-XE-bgp-oper}'
 
-    # Get BGP neighbors based on filter and print out
-    response = router.get(filter=netconf_filter)
-    xml_data = etree.tostring(
+def get_bgp_neighbors(con, filter):
+    '''Function to get BGP neighbors based on filter and return data'''
+
+    response = con.get(filter=filter)
+    data = etree.tostring(
         response.data_ele,
         pretty_print=True
         ).decode()
-    print(xml_data)
-    # print(f"\nBGP neighbors for {device}:\n")
+    # print(xml_data)
+    return data
 
-    # tree = ET.parse(xml_data)
-    # root = tree.getroot()
 
-    root = ET.fromstring(xml_data)
-    print(root.tag, root.attrib, root[0][0][1][0])
+if __name__ == '__main__':
 
-    for neighbor in root[0][0][1][0][2][0].iter(f'{url}neighbor'):
-        print('BGP Neighbor:', neighbor[0].text, neighbor[1][0].text)
+    # Loop through the devices and connect to it
+    for device in devices:
+        connect = device_connect(device)
+
+        # Choose which filter to use
+        if device == "192.168.255.53":
+            yang_type = 'openconfig'
+            url = '{http://openconfig.net/yang/network-instance}'
+        else:
+            yang_type = 'native'
+            url = '{http://cisco.com/ns/yang/Cisco-IOS-XE-bgp-oper}'
+
+        # Open file for NETCONF filter
+        with open(f'filters/{yang_type}_bgp_neighbor_filter.xml') as file:
+            netconf_filter = file.read()
+
+        # Get XML data and read
+        xml_data = get_bgp_neighbors(connect, netconf_filter)
+        root = ET.fromstring(xml_data)
+
+        # Browse through the XML tree and print all neighbors with state
+        print(f'\nBGP neighbors for {device}:')
+        if device == "192.168.255.53":
+            for neighbor in root[0][0][1][0][2][0].iter(f'{url}neighbor'):
+                address = neighbor[0].text
+                state = neighbor[1][0].text
+                print(f'Neighbor {address} -> {state}')
+        else:
+            for neighbor in root[0][0].iter(f'{url}neighbor'):
+                address = neighbor[0].text
+                state = neighbor[1].text
+                print(f'Neighbor {address} -> {state}')
